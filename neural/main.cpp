c2be9c782230fa48212b046758485732672fc144
Krassi <krassimir.krustev@gmail.com>
2022-05-31 06:09:12 -0700

    hello

---
+++ neural/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  neural
+//
+//  Created by Krassi on 5/31/96.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
